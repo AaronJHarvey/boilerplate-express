701c47ae909a01a9b7e8e545979be7e85a0c52b9
Aaron Harvey <Harvey.AaronJ@gmail.com>
2024-07-13 17:00:29 -0400

    uses .env file to capitalize json

--- myApp.js
+++ myApp.js
@@ -1,8 +1,17 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', (req, res) => {
+	if (process.env.MESSAGE_STYLE == 'uppercase') {
+		res.json({ message: 'HELLO JSON' });
+	} else {
+		res.json({ message: 'Hello json' });
+	}
 });
 
 app.use('/public', express.static(__dirname + '/public'));
